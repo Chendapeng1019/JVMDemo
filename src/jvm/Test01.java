a1c510f74d26389f53f827ddc05dd2af0a03fffb
Chendapeng1019 <714491235@qq.com>
2019-10-29 22:07:53 +0800

    jvm 测试

---
+++ src/jvm/Test01.java
@@ -0,0 +1,15 @@
+package jvm;
+
+/**
+ * @ClassName Test01
+ * @Description
+ * @Author chendapeng
+ * @Date 2019/10/27
+ **/
+public class Test01 {
+    static int i=1;
+
+    public static void main(String[] args) {
+        System.out.println(i);
+    }
+}
